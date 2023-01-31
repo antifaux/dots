04669c9fbde818fd14c9119db71059cd9db20d24
antifaux <117097238+antifaux@users.noreply.github.com>
2023-01-30 20:19:33 -0500

    Create preinstalls.sh

---
+++ wayland/preinstalls.sh
@@ -0,0 +1,12 @@
+# Random installs for linuxmobile hyprland
+
+yay -S polkit-kde-agent dunst grimblast rofi rofi-emoji       \
+wl-clipboard wf-recorder wlogout grimblast-git hyprpicker-git hyprpaper-git \
+xdg-desktop-portal-hyprland-git ffmpegthumbnailer tumbler wtype colord      \
+imagemagick swaylock-effects qt5-wayland qt6-wayland ripgrep
+
+# themes
+yay -S catppuccin-gtk-theme-mocha catppuccin-cursors-mocha catppuccin-mocha-grub-theme-git nwg-look
+
+# apps
+yay -S cava pavucontrol ranger zsh starship neovim viewnior noise-suppression-for-voice
