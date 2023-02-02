c437a7e8e5f0118495224616521b4128a1f23636
antifaux <117097238+antifaux@users.noreply.github.com>
2023-02-02 17:45:15 -0500

    Update nerdfontinstalls.sh

--- x11/nerdfontinstalls.sh
+++ x11/nerdfontinstalls.sh
@@ -2,7 +2,8 @@
 
 mkdir -p $HOME/Downloads/nerdfonts/
 cd $HOME/Downloads/
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.1/CascadiaCode.zip
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/CascadiaCode.zip
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/Hack.zip
 unzip '*.zip' -d $HOME/Downloads/nerdfonts/
 rm -rf *.zip
 sudo cp -R $HOME/Downloads/nerdfonts/ /usr/share/fonts/
