d34dca28de641083ae57a14333a730eba0967562
antifaux <117097238+antifaux@users.noreply.github.com>
2023-02-02 17:23:41 -0500

    Create installs.sh

---
+++ x11/installs.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+pacman -Sy \
+archlinux-keyring \
+nomachine \
+sublime-text-4 \
+brave-bin \
+pavucontrol \
+# runelite \
+catppuccin-gtk-theme-mocha \
+catppuccin-cursors-mocha \
+catppuccin-mocha-grub-theme-git \
+spotify \
+
+
+
+# Cause my mouse sucks
+# xinput --set-prop 17 'libinput Accel Speed' -1
+
+# add to wayland
+# nwg-look
